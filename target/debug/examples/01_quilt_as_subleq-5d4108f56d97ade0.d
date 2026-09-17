4c25ae00b4b226d8e0e3cd6a3d60d9854fa384a4
Mavis <Mavis@superinstance.ai>
2026-09-17 07:40:03 +0000

    gitignore: exclude target/
    Initial commit accidentally included the build artifacts. Strip them.
    target/ contains the compiled binaries and dep info; should never be in source control.

--- target/debug/examples/01_quilt_as_subleq-5d4108f56d97ade0.d
+++
@@ -1,5 +0,0 @@
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/repos/quilt-subleq/target/debug/examples/01_quilt_as_subleq-5d4108f56d97ade0.d: examples/01_quilt_as_subleq.rs
-
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/repos/quilt-subleq/target/debug/examples/01_quilt_as_subleq-5d4108f56d97ade0: examples/01_quilt_as_subleq.rs
-
-examples/01_quilt_as_subleq.rs:
--- target/debug/examples/02_subleq_as_quilt-2c0dc3459288bf6c.d
+++
@@ -1,5 +0,0 @@
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/repos/quilt-subleq/target/debug/examples/02_subleq_as_quilt-2c0dc3459288bf6c.d: examples/02_subleq_as_quilt.rs
-
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/repos/quilt-subleq/target/debug/examples/02_subleq_as_quilt-2c0dc3459288bf6c: examples/02_subleq_as_quilt.rs
-
-examples/02_subleq_as_quilt.rs:
